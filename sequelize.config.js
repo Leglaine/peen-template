80f46ade1cd8070b2edd18c331aaa184050b0517
Lexi Wright <ehdubya@hotmail.com>
2024-09-27 22:00:12 -0700

    Configure sequelize, connect to database

---
+++ sequelize.config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        seederStorage: "sequelize"
+    },
+    test: {
+        use_env_variable: "TEST_DATABASE_URL",
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+};
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require("./app");
+const db = require("./api/db/models");
 
 const port = process.env.PORT;
 const host = process.env.BASE_URL;
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server is now running at ${host} on port ${port}`);
+    try {
+        await db.sequelize.authenticate();
+        console.log("Database connection established successfully");
+    } catch (error) {
+        console.error(error);
+    }
 });
